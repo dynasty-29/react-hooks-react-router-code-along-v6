4e83ac5088e1399d057fe155e649985d9a77a727
SonnieCodes <mgathoni.gathoni9@gmail.com>
2025-01-22 20:13:09 +0300

    done

---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function About(){
+    return(
+        <>
+        <header>
+        
+        </header>
+        <main>
+            <h1>This is my about components</h1>
+        </main>
+        </>
+    );
+};
+
+export default About;
